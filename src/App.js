190a204ae08e9ba1effb79022d3a1c408b778f58
Zahra Hosseinpour <112900114+Z-Hp@users.noreply.github.com>
2023-01-31 22:33:53 +0330

    add state and function But something is wrong!!

--- src/App.js
+++ src/App.js
@@ -4,12 +4,22 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Note from "./components/Note";
 import CreateArea from "./components/CreateArea";
+import { useState } from "react";
 
 function App() {
+  const [listItems, setListItems] = useState([]);
+
+  function addItem(titleNote) {
+    setListItems((prevValue) => {
+      return [...prevValue, titleNote];
+    });
+    console.log(listItems);
+  }
+
   return (
     <div className="App">
       <Header />
-      <CreateArea />
+      <CreateArea addItem={addItem} />
       <Note key={1} title="Note title" content="Note content" />
       <Footer />
     </div>
--- src/components/CreateArea.js
+++ src/components/CreateArea.js
@@ -1,11 +1,45 @@
-export default function CreateArea() {
+import { useState } from "react";
+
+export default function CreateArea(props) {
+  const [titleNote, setTitleNote] = useState("");
+
+  function handleChange(event) {
+    setTitleNote(event.target.value);
+  }
+
   return (
-    <div>
-      <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="6" />
-        <button>Add</button>
-      </form>
+    <div className="form">
+      <input
+        name="title"
+        placeholder="Title"
+        type="text"
+        value={titleNote}
+        onChange={handleChange}
+      />
+
+      <textarea
+        name="content"
+        placeholder="Take a note..."
+        rows="6"
+        type="text"
+      />
+      {/* <button
+        onClick={() => {
+          props.addNote(titleNote, contentNote);
+          setTitleNote("");
+          setContentNote("");
+        }}
+      >
+        Add
+      </button> */}
+      <button
+        onClick={() => {
+          props.addItem(titleNote);
+          setTitleNote("");
+        }}
+      >
+        <span>Add</span>
+      </button>
     </div>
   );
 }
